f2c310d62a5cec1204f40ec688c0f83b9f3555d3
Clirim Sadiku <clirim100@gmail.com>
2023-02-22 11:13:39 +0100

    added scrolltotop button along with some minor changes

--- frontend/src/components/Navigator.js
+++ frontend/src/components/Navigator.js
@@ -6,13 +6,13 @@ function Navigator() {
   const location = useLocation();
 
   return (
-    <Navbar bg="light" expand="lg">
-      <NavLink to="/" className="navbar-brand">Home</NavLink>
+    <Navbar className='navbar' expand="lg">
+      <NavLink to="/" className="navbar-brand btn-navlinks">Home</NavLink>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ms-auto">
-          {location.pathname !== '/login' && location.pathname !== '/register' && <NavLink to="/login" className="nav-link">Sign In</NavLink>}
-          {location.pathname !== '/login' && location.pathname !== '/register' && <NavLink to="/register" className="nav-link">Sign Up</NavLink>}
+        <Nav className="ms-auto gap-2">
+          {location.pathname !== '/login' && location.pathname !== '/register' && <NavLink to="/login" className="nav-link btn-navlinks">Sign In</NavLink>}
+          {location.pathname !== '/login' && location.pathname !== '/register' && <NavLink to="/register" className="nav-link btn-navlinks">Sign Up</NavLink>}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
