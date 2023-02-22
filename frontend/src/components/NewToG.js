72072f4a68ea0cf5e21cd13c390c1ceb540aa066
Jannis-web <118267813+Jannis-web@users.noreply.github.com>
2023-02-22 10:51:14 +0100

    new to germany

--- frontend/src/components/NewToG.js
+++ frontend/src/components/NewToG.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './NewToG.css'
 
 export default function NewToG() {
   return (
-    <div>NewToG</div>
+    <div>
+      <h2 className='Headingcenter'>NEW TO GERMANY?</h2>
+      <div class="grid-container">
+        <div class="grid-item lineLeft" >
+          <h1>The Good News</h1>
+          <p>Learning German just got easier</p>
+        </div>
+        <div class="grid-item">
+          <h1>The Better News</h1>
+          <p>You can learn German with others and have fun doing it</p>
+        </div>
+      </div>
+    </div>
   )
 }
