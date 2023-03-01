b5a620c1e3c8eead8be6fde8c9d593e5aa760efe
Clirim Sadiku <clirim100@gmail.com>
2023-03-01 09:34:03 +0100

    users location and people nearby are being fetched

--- frontend/src/components/People-nearby.js
+++ frontend/src/components/People-nearby.js
@@ -1,31 +1,78 @@
-import './components.css'
-import Navigator from './Navigator';
-
-
+import "./components.css";
+import { useState, useEffect } from "react";
+import axiosClient from "./axiosClient";
+import { Roller } from "react-awesome-spinners";
 
 function PeopleNearBy() {
-    return(
-        <div>
-            <select className='nearBySelect'>
-                <option value="5km">5km</option>
-                <option value="10km">10km</option>
-                <option value="15km">15km</option>
-                <option value="+20km">+20km</option>
-            </select>
+  const [users, setUsers] = useState([]);
+  const [distance, setDistance] = useState(5000);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    axiosClient
+    // .get(`/users`)
+      .get(`/users?radius=${distance}`)
+      .then((res) => {
+        // console.log(res.data);
+        // if(res.data.geometry) {
+        //  console.log(res.data)
+        // }else{
+        //   console.log(res.data)
+        // }
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [distance]);
 
-                <div className='nearByCard'>
+  return (
+    <div className="row usersNearby">
+
+    
+      <select
+        className="col-3 nearBySelect"
+        onChange={(e) => setDistance(e.target.value)}
+      >
+        <option value={5000}>5km</option>
+        <option value={10000}>10km</option>
+        <option value={20000}>20km</option>
+        <option value={50000}>50km</option>
+      </select>
+
+    
+      {loading ? (
+          <Roller color="#0e4d4d" />
+          ) : (
+              users.map((user) => {
+                  return (
+                      <div className="userNearby border col-12 d-flex justify-content-between my-5 flex-wrap text-center shadow" key={user._id}>
+              <div className=' col-6 m-5 d-flex flex-column justify-content-around'>
+                <h1>{user.name}</h1>
+                <p>{user.description}</p>
+                <p>Hobbies:{user.hobbies}</p>
+                <p>German Level:{user.germanLevel}</p>
+              </div>
+              <div className='col-4 d-flex flex-column justify-content-center '>
+                <img className="userpic" src={user.pic} alt="userpic" />
+              </div>
+                <button className="button text-white">Send Message</button>
+            </div>
+          );
+        })
+        )}
+</div>
+  );
+}
+export default PeopleNearBy;
+  {/* <div className='nearByCard'>
                     <div className='nearByTop'>
-                        <div className='nearByData'>
-                            <h5>name:`${undefined}`, distance:`${undefined}` away</h5>
-                            <button onClick={undefined} className='messageButton '>Send message</button>
-                        </div>
-                        <p>${}</p>
+                    <div className='nearByData'>
+                    <h5>name:`${undefined}`, distance:`${undefined}` away</h5>
+                    <button onClick={undefined} className='messageButton '>Send message</button>
+                    </div>
+                    <p>${}</p>
                     </div>
                     <div className='nearByImage'>
                         <img className='nearByImage'/>
                     </div>
-                </div>
-        </div>
-    );
-}
-export default PeopleNearBy;
+                </div> */}
